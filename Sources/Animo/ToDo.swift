d9ef7a1935f2f75cb30351a53e4b2f273adeffb9
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-11-30 14:33:57 -0800

    Added Colors To Animo

--- Sources/Animo/ToDo.swift
+++ Sources/Animo/ToDo.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+//MARK:- TODO's
+//Shadow
+//Hex
+//Gradients
+//Alerts
+//Add Image To TextField
+//Keyboard Raises with Notifications
+//Colors
+//
+
+
+//public struct Animo {
+//    
+//    public var test = "This is pretty cool"
+//    public var secondTest = "This is also pretty cool"
+//    public var thirdTest = "Another Another Test"
+//    public var fourthTest = "Final Test"
+//    
+//    public init() {} //Always need to have a public init
+//    
+//}
--- Sources/Animo/UIColor+ext.swift
+++ Sources/Animo/UIColor+ext.swift
@@ -5,4 +5,55 @@
 //  Created by Sako Hovaguimian on 11/30/19.
 //
 
-import Foundation
+import UIKit
+
+@available(iOS 10.0, *)
+
+extension UIColor {
+    
+    //MARK:- LIST OF COLORS I LOVE
+
+    public static let skyBlue = setColor(r: 87, g: 154, b: 230)
+    
+    //MARK:- SETS COLOR WITHOUT THE 255 SYNTAX
+    
+    public static func setColor(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor(displayP3Red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+    //MARK:- USE HEX TO SET COLORS
+    
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+
+    
+
+    
+    
+    
+    
+}
+
