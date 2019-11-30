894764a6957132b27bcc778f0a166209f8e843f9
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-11-30 14:45:09 -0800

    -Added a list of colors

--- Sources/Animo/UIColor+ext.swift
+++ Sources/Animo/UIColor+ext.swift
@@ -14,6 +14,15 @@ extension UIColor {
     //MARK:- LIST OF COLORS I LOVE
 
     public static let skyBlue = setColor(r: 87, g: 154, b: 230)
+    public static let mintGreen = UIColor(hex: "#55efc4")
+    public static let regalPurple = UIColor(hex: "#6c5ce7")
+    public static let blackishGray = UIColor(hex: "#2d3436")
+    public static let purplishBlack = UIColor(hex: "#2B2D42")
+    public static let armyGreen = UIColor(hex: "#646E68")
+    public static let soothingGray = UIColor(hex: "#D7D9D7")
+    public static let imgurGreen = UIColor(hex: "#3E885B")
+    public static let maroon = UIColor(hex: "#EF5B58")
+    
     
     //MARK:- SETS COLOR WITHOUT THE 255 SYNTAX
     
@@ -49,6 +58,7 @@ extension UIColor {
         return nil
     }
 
+    
     
 
     
