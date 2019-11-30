c7718f75b8e23e17b87d5374fc152068c45dcca9
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-11-29 22:25:12 -0800

    -Added Another Property To The List

--- Sources/Animo/Animo.swift
+++ Sources/Animo/Animo.swift
@@ -8,6 +8,7 @@ public struct Animo {
     public var test = "This is pretty cool"
     public var secondTest = "This is also pretty cool"
     public var thirdTest = "Another Another Test"
+    public var fourthTest = "Final Test"
     
     public init() {} //Always need to have a public init
     
