baf01af6486cf12df4de3f5d454841adb2b8bd98
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-11-30 15:35:07 -0800

    -Fixed Bug For Adding Image To Textfield

--- Sources/Animo/UITextField+ext.swift
+++ Sources/Animo/UITextField+ext.swift
@@ -12,12 +12,15 @@ extension UITextField {
     
     //MARK:- ADDING AN IMAGE TO THE RIGHT OF THE TEXTFIELD
     
-    public func attachImage(image: UIImage, width: CGFloat, height: CGFloat) {
+    public func attachImage(image: UIImage) {
         
         let viewPasswordButton = UIButton(type: .custom)
+        
         viewPasswordButton.setImage(image, for: .normal)
-        viewPasswordButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
-        viewPasswordButton.frame = CGRect(x: self.frame.width, y: 0, width: width, height: height)
+        viewPasswordButton.imageView?.contentMode = .scaleAspectFit
+        viewPasswordButton.imageEdgeInsets = UIEdgeInsets(top: 5, left: -16, bottom: 0, right: 0)
+        viewPasswordButton.frame = CGRect(x: self.frame.width, y: 0, width: 17, height: 20)
+        
         self.rightView = viewPasswordButton
         self.rightViewMode = .always
         
