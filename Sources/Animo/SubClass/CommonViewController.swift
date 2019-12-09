4a29d2873ff9da8a15eba43d609bffcdf0b408c3
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-12-09 07:31:20 -0800

    -Added VC class for keyboard helpers

---
+++ Sources/Animo/SubClass/CommonViewController.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sako Hovaguimian on 12/9/19.
+//
+
+import UIKit
+
+public class CommonViewController: UIViewController {
+    
+    //MARK:- RAISING VIEW WHEN TEXTFIELD IS CALLED
+    
+    public var height: CGFloat = -1 //The value of how high the keyboard is raised
+    
+    public func keyboardSetup() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= self.height != -1 ? self.height : keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    
+}
