a6c15aa25917c7ac7e04aefedede72c48bd938d9
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-12-13 09:41:05 -0800

    -Another Storyborded Update

--- Sources/Animo/Extensions/Stoaryboard+ext.swift
+++ Sources/Animo/Extensions/Stoaryboard+ext.swift
@@ -12,7 +12,7 @@ public protocol Storyboarded {
 }
 
 extension Storyboarded where Self: UIViewController {
-    static func instantiate(nameOf storyboard: String) -> Self {
+    public static func instantiate(nameOf storyboard: String) -> Self {
         
         let fullName = NSStringFromClass(self)
         let className = fullName.components(separatedBy: ".")[1]
