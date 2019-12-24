e0477228844759161635ff45d2926544a74627d9
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2019-12-24 09:41:00 -0800

    -Made Protocol Public

--- Sources/Animo/Extensions/String+ext.swift
+++ Sources/Animo/Extensions/String+ext.swift
@@ -12,8 +12,8 @@ import UIKit
 extension String {
     
     //Convert Base64 To Image
-    public func convertBase64ToImage(imageString: String) -> UIImage {
-      let imageData = Data(base64Encoded: imageString, options: Data.Base64DecodingOptions.ignoreUnknownCharacters)!
+    public func convertBase64ToImage() -> UIImage {
+      let imageData = Data(base64Encoded: self, options: Data.Base64DecodingOptions.ignoreUnknownCharacters)!
       return UIImage(data: imageData)!
     }
     
