0e5012bb1cace9dd1e4d6d8ae7d0c3abfa1a7f3a
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2020-04-03 18:38:48 -0700

    -Organized Colors

--- Sources/Animo/Extensions/UIColor+ext.swift
+++ Sources/Animo/Extensions/UIColor+ext.swift
@@ -18,26 +18,40 @@ extension UIColor {
     public static let darkTeal = setColor(r: 0, g: 93, b: 105)
     
     //HEX
-    public static let mintGreen = UIColor(hex: "#55efc4")
-    public static let regalPurple = UIColor(hex: "#6c5ce7")
-    public static let blackishGray = UIColor(hex: "#2d3436")
-    public static let purplishBlack = UIColor(hex: "#2B2D42")
-    public static let armyGreen = UIColor(hex: "#646E68")
-    public static let soothingGray = UIColor(hex: "#D7D9D7")
-    public static let imgurGreen = UIColor(hex: "#3E885B")
-    public static let maroon = UIColor(hex: "#EF5B58")
+    
+    //Blacks
+    public static let notSoBlack = UIColor(hex: "#101010")
+    
+    //Whites
+    public static let notSoWhite = UIColor(hex: "#E6E6E6")
     public static let eggShell = UIColor(hex: "#EAEFD3")
     
-    public static let notSoBlack = UIColor(hex: "#101010")
-    public static let notSoWhite = UIColor(hex: "#E6E6E6")
+    //Grays
+    public static let blackishGray = UIColor(hex: "#2d3436")
+    public static let soothingGray = UIColor(hex: "#D7D9D7")
     
-    public static let lightSalmon = UIColor(hex: "#E9C0B3")
-    public static let muddyBlue = UIColor(hex: "#6F8E8E")
+    //Reds
+    public static let maroon = UIColor(hex: "#EF5B58")
     
-    public static let sexySalmon = UIColor(hex: "#EB3939")
+    //Blues
     public static let darkBlue = UIColor(hex: "#072028")
     public static let midnightTeal = UIColor(hex: "#0092A4")
+    public static let muddyBlue = UIColor(hex: "#6F8E8E")
     public static let lightTeal = UIColor(hex: "#04C3DB")
+    
+    //Purples
+    public static let regalPurple = UIColor(hex: "#6c5ce7")
+    public static let purplishBlack = UIColor(hex: "#2B2D42")
+    
+    //Greens
+    public static let mintGreen = UIColor(hex: "#55efc4")
+    public static let armyGreen = UIColor(hex: "#646E68")
+    public static let imgurGreen = UIColor(hex: "#3E885B")
+    
+    //Pinks
+    public static let lightSalmon = UIColor(hex: "#E9C0B3")
+    public static let sexySalmon = UIColor(hex: "#EB3939")
+    public static let tinderOrange = UIColor(hex: "#F5504A")
     
     
     //MARK:- SETS COLOR WITHOUT THE 255 SYNTAX
