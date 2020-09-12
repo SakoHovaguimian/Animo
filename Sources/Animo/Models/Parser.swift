d61db9c71b95db5a71a8962f2330892346d4c3d0
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2020-09-12 12:20:54 -0700

    -Fixed access control on functions

--- Sources/Animo/Models/Parser.swift
+++ Sources/Animo/Models/Parser.swift
@@ -9,12 +9,12 @@ import Foundation
 
 open class Parser {
 
-    static func encode<T>(_ value: T) throws -> Any where T: Encodable {
+    static public func encode<T>(_ value: T) throws -> Any where T: Encodable {
         let jsonData = try JSONEncoder().encode(value)
         return try JSONSerialization.jsonObject(with: jsonData, options: .allowFragments)
     }
     
-    static func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
+    static public func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
         let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
         return try JSONDecoder().decode(type, from: jsonData)
     }
